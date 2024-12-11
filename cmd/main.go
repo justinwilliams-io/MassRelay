2194d11e8c301968261a1fc74732aee62ce4028e
justinwilliams.dev <justinpwilliams@duck.com>
2024-12-11 17:18:24 -0600

    Updates

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/csv"
 	"fmt"
 	"mass-relay/internal/config"
 	"mass-relay/internal/storage"
@@ -54,7 +55,19 @@ func removeFileFromInProgress(files []string, fileToRemove string) []string {
 	return files
 }
 
-func updateDisplay(totalFiles int, completedFiles int, inProgressFiles []string) {
+func getId(prefixToID map[string]string, filename string) string {
+	returnId := ""
+	for prefix, id := range prefixToID {
+		hyphenatedPrefix := strings.ReplaceAll(strings.ReplaceAll(strings.ReplaceAll(strings.ReplaceAll(prefix, "|", "-"), "/", "-"), ":", "-"), "\"", "-")
+		if strings.HasPrefix(filename, hyphenatedPrefix) {
+			returnId = id
+			break
+		}
+	}
+	return returnId
+}
+
+func updateDisplay(totalFiles int, completedFiles int, inProgressFiles []string, totalBytes, finishedBytes int64, errored []string) {
 	fmt.Print("\033[2J\033[H") // Clear the screen
 
 	fmt.Println("--------------------------------------------------")
@@ -67,9 +80,10 @@ func updateDisplay(totalFiles int, completedFiles int, inProgressFiles []string)
 		fmt.Printf("|   - %s\n", file)
 	}
 	fmt.Println("|")
-	// Add estimated time remaining here, if applicable
-	// fmt.Println("| Estimated Time Remaining: 15 minutes")
-	fmt.Println("| Errors: 0") // Replace 0 with the actual number of errors
+	fmt.Println("| Errored:") // Replace 0 with the actual number of errors
+	for _, file := range errored {
+		fmt.Printf("|   - %s\n", file)
+	}
 	fmt.Println("--------------------------------------------------")
 
 	progressBarLength := 50
@@ -77,17 +91,36 @@ func updateDisplay(totalFiles int, completedFiles int, inProgressFiles []string)
 	filledLength := int(progressPercentage * float64(progressBarLength))
 	progressBar := strings.Repeat("=", filledLength) + strings.Repeat("-", progressBarLength-filledLength)
 	fmt.Printf("\nProgress: [%s] %.2f%%\n", progressBar, progressPercentage*100)
+
+	remainingTime := time.Duration(totalBytes-finishedBytes) * 5000
+	fmt.Printf("Estimated Time Remaining: %s\n", remainingTime.String())
 }
 
 func main() {
 	isSimulation := true
 
+	csvFile, err := os.Open("ids.csv")
+	if err != nil {
+		panic(err)
+	}
+	defer csvFile.Close()
+
+	reader := csv.NewReader(csvFile)
+	records, err := reader.ReadAll()
+	if err != nil {
+		panic(err)
+	}
+
+	prefixToID := make(map[string]string)
+	for _, record := range records {
+		prefixToID[record[0]] = record[1]
+	}
+
 	var files []string
-	var err error
 	if isSimulation {
 		files, _ = storage.ScanFiles("./simulation-files")
 	} else {
-		files, err = storage.ScanFiles(".")
+		files, err = storage.ScanFiles("./simulation-files")
 		if err != nil {
 			fmt.Println("Error scanning files:", err)
 			return
@@ -97,6 +130,7 @@ func main() {
 	totalFiles := len(files)
 	completedFiles := 0
 	inProgressFiles := make([]string, 0, len(files))
+	errored := make([]string, 0, len(files))
 
 	configDir, _ := os.UserConfigDir()
 	cfg, err := config.ReadConfig(filepath.Join(configDir, "mass-relay", "config.yaml"))
@@ -115,6 +149,8 @@ func main() {
 		totalBytes += fileInfo.Size()
 	}
 
+	finishedBytes := int64(0)
+
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Minute)
 	defer cancel()
 
@@ -122,18 +158,18 @@ func main() {
 	sem := make(chan struct{}, cfg.MaxConcurrentUplaods)
 
 	updateChan := make(chan struct{}, 1)
-    go func() {
-        for {
-            select {
-            case <-updateChan:
-                updateDisplay(totalFiles, completedFiles, inProgressFiles)
-            case <-ctx.Done():
-                return
-            }
-        }
-    }()
+	go func() {
+		for {
+			select {
+			case <-updateChan:
+				updateDisplay(totalFiles, completedFiles, inProgressFiles, totalBytes, finishedBytes, errored)
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
 
-    updateChan <- struct{}{}
+	updateChan <- struct{}{}
 
 	for _, file := range files {
 		sem <- struct{}{}
@@ -141,20 +177,27 @@ func main() {
 		go func(file string) {
 			defer func() {
 				<-sem
-				wg.Done()
 
+				fileInfo, _ := os.Stat(file)
+				oppId := getId(prefixToID, fileInfo.Name())
+				if oppId == "" {
+					errored = append(errored, file)
+				}
+				inProgressFiles = removeFileFromInProgress(inProgressFiles, fileInfo.Name()+" - "+oppId)
 				completedFiles++
-				inProgressFiles = removeFileFromInProgress(inProgressFiles, file)
+				finishedBytes += fileInfo.Size()
 				updateChan <- struct{}{}
+				wg.Done()
 			}()
 
-			inProgressFiles = append(inProgressFiles, file)
+			fileInfo, _ := os.Stat(file)
+			fileSize := fileInfo.Size()
+			oppId := getId(prefixToID, fileInfo.Name())
+			inProgressFiles = append(inProgressFiles, fileInfo.Name()+" - "+oppId)
 			updateChan <- struct{}{}
 
 			if isSimulation {
-				fileInfo, _ := os.Stat(file)
-				fileSize := fileInfo.Size()
-				time.Sleep(time.Duration(fileSize) * 10000)
+				time.Sleep(time.Duration(fileSize) * 5000)
 			} else {
 				err := UploadFile(ctx, file, cfg.RemoteURL, cfg.Token)
 				if err != nil {
@@ -165,6 +208,6 @@ func main() {
 	}
 
 	wg.Wait()
-    close(updateChan)
-	fmt.Println("All files uploaded successfully!")
+	close(updateChan)
+	fmt.Println("Migration Complete!")
 }
